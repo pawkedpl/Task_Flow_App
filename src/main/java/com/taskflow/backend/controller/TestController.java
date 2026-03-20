c7abbe559929514e8dbfe9fad3aed7ec994559db
Paweł Kędzierski <147321628+pawkedpl@users.noreply.github.com>
2026-03-20 02:48:58 +0100

    neext

---
+++ src/main/java/com/taskflow/backend/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.taskflow.backend.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Logged in as: " + SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+}
